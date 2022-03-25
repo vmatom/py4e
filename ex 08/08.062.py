5980694acb462f56746f3367c9d26c8575977bf9
vmatom <vmatom@gmail.com>
2022-03-25 16:23:52 +0300

    Added def function middle. Which append list by 1st and last parts of the lists

--- ex 08/08.062.py
+++ ex 08/08.062.py
@@ -8,5 +8,17 @@ while numb != "Done":
         print("not a number")
     else:
         break
+mid_list=[]
+def middle(t,k):
+    k.append(t[0])
+    k.append(t[-1])
+middle(include,mid_list)
+print(mid_list)
 print("maximum: ", max(include))
 print("minimum: ", min(include))
+print(include)
+def chop(t):
+    del t[0]
+    del t[-1]
+chop(include)
+print(include)
