4afb7cdc1db85f3f35384aa970873af839f5427b
vmatom - Vladislav <99231352+vmatom@users.noreply.github.com>
2022-05-17 17:43:44 +0300

    12.02 exercise. changed typo, and other minor errors

--- ex 12 HTTP Networking/ex.12.02.py
+++ ex 12 HTTP Networking/ex.12.02.py
@@ -3,27 +3,28 @@
 # count the total number of characters and display the count of the number of characters at the end of the document.
 # urllib library in python that does all the socket work for us and makes web pages looks like a file
 # http://www.py4inf.com/code/mbox-short.txt
-import urllib.request, urllib.error, urllib.parse
-inpadr = input("Type URL you need to connect to: ")
-fhand= urllib.request.urlopen(inpadr)
-counts=dict()
-countschar = dict()
-countsword=0
-text=""
+import urllib.request
+import urllib.parse
+inp_adress = input("Type URL you need to connect to: ")
+fhand = urllib.request.urlopen(inp_adress)
+counts = dict()
+counts_char = dict()
+counts_word = 0
+text = ""
 try:
     for line in fhand:
-        words =line.decode().split()
-        line=line.decode().rstrip()
-        text=text+line
-        countsword=countsword+len(words)-1
+        words = line.decode().split()
+        line = line.decode().rstrip()
+        text = text+line
+        counts_word = counts_word+len(words)-1
         for word in words:
             characters = word
             counts[word] = counts.get(word, 0) + 1
             for character in characters:
-                countschar[character] = countschar.get(character, 0)+1
+                counts_char[character] = counts_char.get(character, 0)+1
 except:
     print("ended")
-# print(counts,"\n", countschar,"\n", countsword)
+# print(counts,"\n", counts_char,"\n", counts_word)
 # print(text, len(text))
-print(countschar)
-print(text[:3000], "\n ammount of characters - ", len(text))
+print(counts_char)
+print(text[:3000], "\n amount of characters - ", len(text))
