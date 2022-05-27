8180d63daecca484bb069322c1e2734daa70b7e1
vmatom - Vladislav <99231352+vmatom@users.noreply.github.com>
2022-05-28 01:13:42 +0300

    13.04 Exercise. Finding code of the country of the place. If there is no country. Program say "that there is no country here"

---
+++ ex 13 Web services/ex.13.04.py
@@ -0,0 +1,60 @@
+# Exercise 1: Change either geojson.py or geoxml.py to print out the two-character country code from the retrieved data.
+# Add error checking so your program does not traceback if the country code is not there. Once you have it working,
+# search for “Atlantic Ocean” and make sure it can handle locations that are not in any country.
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+    try:
+        #we find exactly adress components and finding part of dict with country
+        cc_code =next(x for x in js['results'][0]['address_components'] if x["types"] == ["country", "political"])
+        print("country code is : ", cc_code["short_name"])
+
+    except:
+        print("there is no country here")
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
